e228175dc3126c11411e15cf62dced1fba80e3b2
Ishraq Hasan <ishraq10199@gmail.com>
2022-08-05 17:11:53 +0600

    fixed flicker rate in 01_a

--- examples/01/variant_a/shaders/fragment_shader.js
+++ examples/01/variant_a/shaders/fragment_shader.js
@@ -4,7 +4,8 @@ const fragment = `
 uniform float u_time;
 
 void main() {
-    float val = abs(sin(u_time));
+    float flicker_rate = 8.0;
+    float val = abs(sin(u_time * flicker_rate));
     gl_FragColor = vec4(val, 0.0, val, 1.0);
 }
 
